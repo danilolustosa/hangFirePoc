5d6e5056a33b6eb110ac8f0e429d8e4fb5157806
Danilo Lustosa <danilolustosa@gmail.com>
2021-05-20 12:17:30 -0300

    First commit

---
+++ Controllers/HangFireController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFirePOC.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HangFireController : ControllerBase
+    {
+
+        private readonly ILogger<HangFireController> _logger;
+
+        public HangFireController(ILogger<HangFireController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ Process.cs
@@ -0,0 +1,17 @@
+﻿using Hangfire;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFirePOC
+{
+    public static class Process
+    {
+        public static void RecurringJobs()
+        {
+            var division = new Division();
+            RecurringJob.AddOrUpdate(() => division.DivisionRandom(), Cron.Minutely());
+        }
+    }
+}
